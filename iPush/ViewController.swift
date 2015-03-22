22778cd1c19500b1101c9939219294d25a5ba2b6
Heverton Rodrigues <heverton25@gmail.com>
2015-03-21 21:57:13 -0300

    Initial Commit

---
+++ iPush/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iPush
+//
+//  Created by Heverton on 21/03/15.
+//  Copyright (c) 2015 AuroraConsult. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
